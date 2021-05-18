29e0e8f7c5363f8a0136a94a0815e0a7ee022cdf
Valery Nevzorov <masterpiece.nevzorov@gmail.com>
2021-05-18 12:01:53 +0300

    Initial Commit

---
+++ In-Place/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  In-Place
+//
+//  Created by Иван Карамазов on 18.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
